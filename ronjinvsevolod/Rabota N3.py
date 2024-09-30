6dc5f7c7afd557d3cc156f5ec43a139344e3634b
ITKidsParnas <itkids.parnas@gmail.com>
2024-09-30 18:53:03 +0300

    30.09

---
+++ ronjinvsevolod/Rabota N3.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    print("Гони касарь")
+A=0
+while A<5:
+    print(A)
+    A+=1
---
+++ ronjinvsevolod/Rabota N4.py
@@ -0,0 +1,6 @@
+import time
+vremia=int(input("Сколько секунд? "))
+while vremia !=0:
+    print(vremia)
+    vremia-=1
+    time.sleep(1)
