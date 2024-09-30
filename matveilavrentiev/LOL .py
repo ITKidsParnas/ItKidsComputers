ca1d3995694a17136472b0b919a5a948490a5712
ITKidsParnas <itkids.parnas@gmail.com>
2024-09-30 18:52:20 +0300

    30.09

---
+++ matveilavrentiev/LOL .py
@@ -0,0 +1,8 @@
+for i  in  range(100000):
+    print("го  играть")
+
+a = 0
+
+while a < 1000001:
+    print(a) 
+    a += 1
---
+++ matveilavrentiev/LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL.py
@@ -0,0 +1,8 @@
+import   time
+
+vremia =  int(input("сколько  секунд"))
+
+while  vremia != 0:
+    print(vremia)
+    vremia  -= 1
+    time.sleep(1)
