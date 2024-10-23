25a174299523a00f646806992ca7e41996fca41b
ITKidsParnas <itkids.parnas@gmail.com>
2024-10-23 19:16:46 +0300

    23.10.24

---
+++ Боярчук Люция/Unity/Lucia/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   private void Start()
+   {
+        print("Hello");
+   }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        print("AAAA");
+    }
+}
