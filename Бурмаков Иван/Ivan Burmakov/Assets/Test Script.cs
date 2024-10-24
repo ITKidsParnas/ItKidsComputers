6b145f906b3235fe80b7719e21714583f801c1c2
ITKidsParnas <itkids.parnas@gmail.com>
2024-10-24 20:17:38 +0300

    24.10

---
+++ Бурмаков Иван/Ivan Burmakov/Assets/Test Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        print("Hello");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        print("AAAA");
+    }
+}
