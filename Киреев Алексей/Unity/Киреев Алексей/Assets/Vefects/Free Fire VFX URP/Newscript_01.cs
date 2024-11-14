c1849a40dd63718e5dcf027e283d40ec6f1e0fbb
ITKidsParnas <itkids.parnas@gmail.com>
2024-11-14 20:21:07 +0300

    14.11.2024

---
+++ Киреев Алексей/Unity/Киреев Алексей/Assets/Vefects/Free Fire VFX URP/Newscript_01.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Newscript_01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("welcome to durka");              
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
