d7383dc0e2b455c133f06d93e0ed533713ceb61f
ITKidsParnas <itkids.parnas@gmail.com>
2024-11-15 19:13:53 +0300

    merge  Богодухов Сергей=> to itk1

---
+++ Богодухов Сергей/Богодухов Сергей/Assets/chicen.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chicen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+   
+    }
+
+    // Updaed once per frame
+    void Update()
+    {
+        print("zoppppppppppppaaaaaaaaaa");
+    }
+}
