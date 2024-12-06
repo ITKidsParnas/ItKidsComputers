6193d7bd0890ae9a8b266e4c3b2e563bd919a4ac
ITKidsParnas <itkids.parnas@gmail.com>
2024-12-06 19:33:54 +0300

    06.12.24

--- Zhokov Sergey AYE/urok15.py
+++ Zhokov Sergey AYE/urok15.py
@@ -1,20 +1,108 @@
-#7826088357:AAEdQiSU5OE16CQQtJZTcstYoMKvWcIWv_4
-
+import uuid
+import os
+import speech_recognition as sr
+import ctypes
+import pyogg
+import numpy as np
+import wave
 import telebot
+import translate
+from moviepy.audio.io.AudioFileClip import AudioFileClip
 
 bot = telebot.TeleBot("7826088357:AAEdQiSU5OEl6CQQtJZTcstYoMKvWcIWv_4", parse_mode=None)
+language = 'ru_RU'
+r = sr.Recognizer()
+translator = translate.Translator(to_lang="ru")
+
+def recognise(filename):
+    with sr.AudioFile(filename) as source:
+        audio_text = r.listen(source)
+        try:
+            text = r.recognize_google(audio_text,language=language)
+            print('Перевожу аудио в текст')
+            print(text)
+            return text
+        except:
+            print("Ошибка,попробуйте ссннова")
+            return"Ошибка,попробуйте снова"
+
+
+def convert_opus_to_wav(input_file, output_file):
+    opus_file = pyogg.OpusFile(input_file)
+    channels = opus_file.channels
+    samplerate = opus_file.frequency
+    buffer_lenght = opus_file.buffer_length
+    buffer_ptr = opus_file.buffer
+
+    pcm_data = np.ctypeslib.as_array((ctypes.c_short * buffer_lenght).from_address(ctypes.addressof(buffer_ptr.contents)))
+
+    with wave.open(output_file, 'wb') as wav_file:
+        wav_file.setnchannels(channels)
+        wav_file.setsampwidth(2)
+        wav_file.setframerate(samplerate)
+        wav_file.writeframes(pcm_data.tobytes())
+
+
+def convert_mp4_to_wav(input_file, output_file):
+    audio = AudioFileClip(input_file)
+    audio.write_audiofile(output_file)
+    
+
 
 @bot.message_handler(commands=["start","help"])
 def command_response(message):
     print(message.chat.username, ":", message.text)
     if message.text == "/start":
-        bot.reply_to(message, " мандаринка")
+        bot.reply_to(message, " мандаринка,перешли мне глосовое сообщение и я его расшифрую.")
+    if message.text == "/help":
+        bot.reply_to(message, "ЧО ты думал?В сказку попал? @#& тебе!")
 
 @bot.message_handler(content_types=['text'])
 def text_response(message):
     print(message.chat.username, ":", message.text)
+    if "переведи" or "Переведи" in message.text:
+        text = message.text
+        text.replace("переведи","", 1)
+        t_text = translator.translate(text)
+        bot.reply_to(message, t_text)
 
-bot.infinity_polling()s
+
+
+@bot.message_handler(content_types=['voice'])
+def voice_proc(message):
+    filename = str(uuid.uuid4())
+    file_name_full = filename + ".ogg"
+    file_name_full_converted = filename + ".wav"
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    convert_opus_to_wav(file_name_full,file_name_full_converted)
+    text = recognise(file_name_full_converted)
+    bot.reply_to(message, text)
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
+
+
+@bot.message_handler(content_types=['video_note'])
+def video_note_proccesing(message):
+    filename = str(uuid.uuid4())
+    file_name_full = filename + ".mp4"
+    file_name_full_converted = filename + ".wav"
+    file_info = bot.get_file(message.video_note.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    convert_mp4_to_wav(file_name_full,file_name_full_converted)
+    text = recognise(file_name_full_converted)
+    bot.reply_to(message, text)
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
+
+
+Ы
+bot.infinity_polling()
+
 
 
 
--- Волков Светослав/Volkov Svetoslav/Assets/by.cs
+++ Волков Светослав/Volkov Svetoslav/Assets/by.cs
@@ -2,27 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class by : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
-
+    private void OnCollisionExit(Collision collision)
+    {
+        GetComponent<MeshRenderer>().materials[0].color = Color.red;
+    }
     private void OnCollisionEnter(Collision collision)
     {
+
         GetComponent<MeshRenderer>().materials[0].color = Color.green;
-            
-            
-            
-            
-            
 
     }
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 }
