9da4f4a932301acebb45526ab7b63679aef800d7
ITKidsParnas <itkids.parnas@gmail.com>
2025-05-02 19:05:31 +0300

    02.05.2025

--- розанов марк/unity/марк розанов/Assets/Scripts/menu.cs
+++ розанов марк/unity/марк розанов/Assets/Scripts/menu.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class menu : MonoBehaviour
 {
+    public GameObject menuPanel;
     public void RestartButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void Quit() 
+    {
+
+    }
+    public void Continue()
+    {
+        menuPanel.SetActive(false);
     }
     // Start is called before cethe first frame update
     void Start()
